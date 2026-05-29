de52e6b340f8d92bd67bb0c7372141c0cf308f0f
vishnu-dev27 <sillijet310@gmail.com>
2026-05-29 10:08:39 +0000

    if-statements.py

---
+++ if-statements.py
@@ -0,0 +1,22 @@
+#1. Basic if statement
+
+age = 21
+if age >=18:
+	print("you are eligible to vote!")
+
+#2. Statement outside the if block
+
+number = 3
+if number> 0:
+	print("positive number")
+print("this line is always printed")
+
+#3. Nested if statements
+
+marks = 87
+if marks >= 50:
+	print("you have passed this test!")
+if marks >= 85:
+	print("excellent performance")
+
+
