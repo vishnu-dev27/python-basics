fd016ac93d8a002439749da86d0b26068215800f
vishnu-dev27 <sillijet310@gmail.com>
2026-05-30 16:00:56 +0000

    basic-for-loops.py

---
+++ basic-for-loops.py
@@ -0,0 +1,17 @@
+# Basic for loops
+
+# Range(stop)
+for i in range(7):
+	print(i)
+	print("Done!")
+
+# Range(start,stop)
+for i in range(2,6):
+	print(i)
+	print("Done!")
+
+# Range(start,stop,step)
+for i in range(1,10,2):
+	print(i)
+	print("Done!")
+
