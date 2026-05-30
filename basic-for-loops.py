7747f4a1e7219624482409fc1000c70306515f5d
vishnu-dev27 <sillijet310@gmail.com>
2026-05-30 22:26:34 +0530

    Update basic-for-loops.py

--- basic-for-loops.py
+++ basic-for-loops.py
@@ -14,4 +14,13 @@ for i in range(2,6):
 for i in range(1,10,2):
 	print(i)
 	print("Done!")
+	
+# Reversing a range using negative step function
+for i in range(5,0,-1):
+	print(i)
+	print ("Done!")
 
+# Reversing a range using reversed( ) function
+for i in reversed(range(1,6,1)):
+	print(i)
+	print("Done!")
