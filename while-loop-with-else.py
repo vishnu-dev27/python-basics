1229f32a87f52bf2a97ab8ca0ee28e1cc2f91818
vishnu-dev27 <sillijet310@gmail.com>
2026-06-02 07:08:19 +0000

    while-loop-with-else.py

---
+++ while-loop-with-else.py
@@ -0,0 +1,13 @@
+fruits = ['banana','figs','apple','jackfruit']
+fruit_len = len(fruits)
+index = 0
+fruit-found == False
+ while index < len(fruits):
+     if fruits[index] == 'Orange':
+         fruit-found is true
+         print("Orange is available.")
+         break
+         index+=- 1
+         if not fruit-found:
+             print("Orange is not available.")
+
