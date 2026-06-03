08a3973004b7f365e2ebb4685a451357bad1aa0f
vishnu-dev27 <sillijet310@gmail.com>
2026-06-03 08:42:15 +0000

    lists.py

---
+++ lists.py
@@ -0,0 +1,26 @@
+#1. Basic Lists (or) Single-dimensional lists
+
+numbers_list = [8,16,24,32]
+print(numbers_list)
+
+#2. Accessing Values from a Single-dimensional list
+
+numbers_list = [8,16,24,32]
+print(numbers_list[2])
+
+#3. Accessing Negative indexing Values from a Single-dimensional list
+
+numbers_list = [8,16,24,32]
+print(numbers_list[-1])
+
+#4. Multi-dimensional lists
+
+my_list1 = [[16,24,32], "Meso soup", [40,48,56]]
+print(my_list1)
+
+#5. Accessing Values from a multi-dimensional list
+
+my_list1 = [[16,24,32], "Meso soup", [40,48,56]]
+print(my_list1[0][1])
+
+
