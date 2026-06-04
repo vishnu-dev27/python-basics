98f52b376207c52a4d95737829f5acf8de41ba8f
vishnu-dev27 <sillijet310@gmail.com>
2026-06-04 05:55:38 +0000

    lists_methods.py

---
+++ lists_methods.py
@@ -0,0 +1,40 @@
+#1. Append( ) method of lists
+languages = ['c','cpp','java']
+languages
+languages.append('python')
+languages
+
+#2. Insert( ) method of lists
+languages = ['c','cpp','java','javascript','HTML','CSS']
+lanugages.insert(3,'python')
+languages
+
+#3. Extend( ) method of lists
+list1 = ['c','cpp','java']
+list2 = ['javascript','HTML','CSS']
+list1.extend(list2)
+languages
+
+#4. Input( ) method of lists
+name = input()
+Carryiminati
+name
+
+#5. Input( ) method with message of lists
+number = input('enter a number: ')
+enter a number: 20
+number
+'20'
+
+#6. Typecasting the user input
+number = int(input("enter a number: "))
+enter a number: 88
+number
+'88'
+
+number = int(input("enter a number: "))
+enter a number: 90
+number
+'90'
+int(number)
+'90'
