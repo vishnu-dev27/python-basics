a92a73b9cd0d70e20f5f4bd3a80aee89be9de7b0
vishnu-dev27 <sillijet310@gmail.com>
2026-06-05 10:20:17 +0530

    Update while-loop-with-else.py

--- while-loop-with-else.py
+++ while-loop-with-else.py
@@ -1,13 +1,13 @@
 fruits = ['banana','figs','apple','jackfruit']
 fruit_len = len(fruits)
 index = 0
-fruit-found == False
+fruit-found = False
  while index < len(fruits):
      if fruits[index] == 'Orange':
-         fruit-found is true
+         fruit-found = True
          print("Orange is available.")
          break
          index+=- 1
-         if not fruit-found:
+         if not fruits_found:
              print("Orange is not available.")
 
