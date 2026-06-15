bb375a31b01b70ec12ee046bee3de1c1038fb553
vishnu-dev27 <sillijet310@gmail.com>
2026-06-15 05:55:26 +0000

    functions_1.py

---
+++ functions_1.py
@@ -0,0 +1,35 @@
+#1. Simple Function
+
+def greet():
+    print("Hello!")
+greet()
+
+
+#2. Functions with Parameters
+
+def greet(name):
+    print("Hello!",name)
+greet("Vishnu")
+
+
+#3. Functions with return Values
+
+def add(a,b)
+    return a+b
+result = add(2,3)
+print(result)
+
+
+#4. functions with default parameters
+
+def greet(name="Guest"):
+    print("Hello!",name)
+greet()
+greet("Vishnu")
+
+
+#5. Functions with Keyword Arguements
+
+def greet(name,age):
+    print(name,age)
+info(name="Vishnu",age=18)
