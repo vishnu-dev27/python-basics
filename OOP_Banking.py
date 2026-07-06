b3ad82b4cd6f795be37ed8c16c1dd297a476db58
vishnu-dev27 <sillijet310@gmail.com>
2026-07-06 19:06:06 +0530

    Update OOP_Banking.py

--- OOP_Banking.py
+++ OOP_Banking.py
@@ -4,6 +4,19 @@ class Account:
         self.customer_id = customer_id
         self.balance = balance
         self.transactions = []
+    @property
+    def balance(self):
+        return self._balance
+    @balance.setter
+    def balance(self,amount):
+        if amount >= 0:
+            self._balance = amount
+        else:
+            print("Balance Cannot be Negative!")
+    @balance.deleter
+    def balance(self):
+        print(f"{self.name}'s Balance: record deleted")
+        del self._balance
     def check_balance(self):
         print(f"{self.name} Bank Balance $ : {self.balance}")
     def deposit(self,amount):
@@ -39,6 +52,11 @@ class CurrentAccount(Account):
 c1 = CurrentAccount("Vishnu",1265,56890)
 c2 = CurrentAccount("Nick",1266,46890)
 c3 = CurrentAccount("Chris",1267,66890)
+print(c1.balance)
+c1.balance = 56890
+print(c1.balance)
+c1.balance = -900
+print(c1.balance)
 c1.check_balance()
 c1.deposit(2190)
 c1.withdraw(3247)
@@ -47,6 +65,11 @@ c1.check_balance()
 c1.Home_Loan()
 c1.show_transactions()
 print("--------------------")
+print(c2.balance)
+c2.balance = 46890
+print(c2.balance)
+c2.balance = -1000
+print(c2.balance)
 c2.check_balance()
 c2.deposit(2190)
 c2.withdraw(3247)
@@ -55,6 +78,11 @@ c2.check_balance()
 c2.business_Loan()
 c2.show_transactions()
 print("--------------------")
+print(c3.balance)
+c3.balance = 66890
+print(c3.balance)
+c3.balance = -1200
+print(c3.balance)
 c3.check_balance()
 c3.deposit(2190)
 c3.withdraw(3247)
